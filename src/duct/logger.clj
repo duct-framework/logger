a1dab559cc03dcdd23dc59cf12c058196fc650de
James Reeves <jreeves@weavejester.com>
2017-04-09 19:18:36 +0100

    Copy logger code from duct/core
    Using commit 849ed83727b58abe2e12e1f3db46e5f3320edc2f

--- src/duct/logger.clj
+++ src/duct/logger.clj
@@ -1,6 +1,18 @@
 (ns duct.logger)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Logger
+  "Protocol for abstracting logging. Used by the log macro."
+  (-log [logger level ns-str file line event data]))
+
+(extend-protocol Logger
+  nil
+  (-log [_ _ _ _ _ _ _]))
+
+(defn- log-form [logger level event data form]
+  `(p/-log ~logger ~level ~(str *ns*) ~*file* ~(:line (meta form)) ~event ~data))
+
+(defmacro log
+  "Log an event and optional data structure at the supplied severity level
+  using a logger that implements the Logger protocol."
+  ([logger level event]      (log-form logger level event nil &form))
+  ([logger level event data] (log-form logger level event data &form)))
