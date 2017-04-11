df1c11e4b8259d6fe6fc787efdb6499dd4fb5a6b
James Reeves <jreeves@weavejester.com>
2017-04-11 19:21:39 +0100

    Add Codox for API documentation

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox {:output-path "codox"
+          :metadata {:doc/format :markdown}
+          :source-uri
+          "https://github.com/duct-framework/logger/blob/{version}/{filepath}#L{line}"})
