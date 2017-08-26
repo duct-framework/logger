766c2d8da7f99b31d173bed9cc189ef989b0efc5
James Reeves <jreeves@weavejester.com>
2017-08-26 15:37:25 +0100

    Add delayed UUID argument to logger

--- src/duct/logger.clj
+++ src/duct/logger.clj
@@ -3,17 +3,26 @@
 
 (defprotocol Logger
   "Protocol for abstracting logging. Used by the log macro."
-  (-log [logger level ns-str file line event data]))
+  (-log [logger level ns-str file line id event data]))
 
 (extend-protocol Logger
   nil
-  (-log [_ _ _ _ _ _ _]))
+  (-log [_ _ _ _ _ _ _ _]))
 
 (defn- log-form [logger level event data form]
-  `(-log ~logger ~level ~(str *ns*) ~*file* ~(:line (meta form)) ~event ~data))
+  `(-log ~logger
+         ~level
+         ~(str *ns*) ~*file* ~(:line (meta form))
+         (delay (java.util.UUID/randomUUID))
+         ~event ~data))
 
 (defmacro log
   "Log an event and optional data structure at the supplied severity level
   using a logger that implements the Logger protocol."
   ([logger level event]      (log-form logger level event nil &form))
   ([logger level event data] (log-form logger level event data &form)))
+
+(def logger
+  (reify Logger
+    (-log [_  level ns-str file line id event data]
+      [level ns-str file line id event data])))
--- test/duct/logger_test.clj
+++ test/duct/logger_test.clj
@@ -7,16 +7,20 @@
   (is (str/blank? (with-out-str (logger/log nil :info ::test1))))
   (is (str/blank? (with-out-str (logger/log nil :warn ::test2 {:foo "bar"})))))
 
-(defrecord AtomLogger [a]
+(defrecord AtomLogger [a ids]
   logger/Logger
-  (-log [logger level ns-str file line event data]
-    (swap! a conj [level ns-str file line event data])))
+  (-log [logger level ns-str file line id event data]
+    (swap! a conj [level ns-str file line event data])
+    (swap! ids conj (force id))))
 
 (deftest log-test
   (let [logs   (atom [])
-        logger (->AtomLogger logs)]
+        ids    (atom [])
+        logger (->AtomLogger logs ids)]
     (logger/log logger :info ::t1)
     (logger/log logger :warn ::t2 {:x "y"})
     (is (= @logs
-           [[:info "duct.logger-test" "duct/logger_test.clj" 18 ::t1 nil]
-            [:warn "duct.logger-test" "duct/logger_test.clj" 19 ::t2 {:x "y"}]]))))
+           [[:info "duct.logger-test" "duct/logger_test.clj" 20 ::t1 nil]
+            [:warn "duct.logger-test" "duct/logger_test.clj" 21 ::t2 {:x "y"}]]))
+    (is (= (count @ids) (count (distinct @ids))))
+    (is (every? (partial instance? java.util.UUID) @ids))))
