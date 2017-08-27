a7074fa76d3113bcfe1242ecef4b57739a771a75
James Reeves <jreeves@weavejester.com>
2017-08-27 20:16:25 +0100

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/logger "0.1.1"
+(defproject duct/logger "0.2.0"
   :description "Logging library for the Duct framework"
   :url "https://github.com/duct-framework/logger"
   :license {:name "Eclipse Public License"
