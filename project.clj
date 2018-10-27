711ac9e725a0c637a1ba4743d2dc7778f50e6a81
James Reeves <jreeves@weavejester.com>
2018-10-27 13:13:58 +0100

    Update Clojure dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/logger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox {:output-path "codox"
           :metadata {:doc/format :markdown}
