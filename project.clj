0f138157373eaa07f4a6ebb4fc536a30847a190e
James Reeves <jreeves@weavejester.com>
2024-10-25 16:24:12 +0100

    Update Codox plugin

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-codox "0.10.8"]]
   :codox {:output-path "codox"
           :metadata {:doc/format :markdown}
           :source-uri
